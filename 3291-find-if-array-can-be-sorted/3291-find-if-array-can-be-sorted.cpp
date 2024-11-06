7577df8204d6c1e43e38a44aa5d0db1267bc1b55
Keerthan Reddy Kolli <112936374+kkr-97@users.noreply.github.com>
2024-11-06 10:01:09 +0530

    Time: 0 ms (100%), Space: 31.3 MB (63.03%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int isSetBitsEqual(int a, int b){
+        int cnt1 = 0;
+        int cnt2 = 0;
+        while(a > 0 || b > 0){
+            if(a > 0){
+                cnt1++;
+                a = a & (a - 1);
+            }
+            if(b > 0){
+                cnt2++;
+                b = b & (b-1);
+            }
+        }
+        return cnt1 == cnt2;
+    }
+
+    bool canSortArray(vector<int>& nums) {
+        int prevMin = INT_MAX;
+        int prevMax = INT_MIN;
+        int i = 0, j = 0, l = nums.size();
+        while(j < l){
+            int curMax = INT_MIN;
+            int curMin = INT_MAX;
+            while(j < l && isSetBitsEqual(nums[i], nums[j])){
+                curMax = max(nums[j], curMax);
+                curMin = min(curMin, nums[j]);
+                j++;
+            }
+            if(i != 0){
+                if(prevMax > curMax || prevMin > curMin || prevMax > curMin) return false;
+            }
+            prevMin = curMin;
+            prevMax = curMax;
+            i = j;
+
+        }
+        return true;
+    }
+};
