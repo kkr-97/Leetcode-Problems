678ada2bd6825bbe8b071f6cd602ab77497228bd
Keerthan Reddy Kolli <112936374+kkr-97@users.noreply.github.com>
2024-11-09 12:57:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (52.8%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long current = x;  
-        for (int i = 1; i < n; i++) {
-            current = ((current + 1) | x);
+        long long targetExt = n-1;
+        long long ans = x;
 
-            while ((current & x) != x) {
-                current++;
+        int xBit = 0, nBit = 0;
+        
+        while(xBit < 64){
+            if((ans>>xBit)&1){
+                xBit++;
+                continue;
             }
+            if((targetExt>>nBit)&1){
+                ans = ans | (1LL << xBit);
+            }
+            nBit++;
+            xBit++;
         }
-
-        return current;
+        return ans;
     }
 };
